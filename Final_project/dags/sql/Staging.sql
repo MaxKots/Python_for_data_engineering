e7f509aafcd2ef454016da7d0c3ee76bf3290b14
Maxim Kotsyuba <knoll548@gmail.com>
2025-01-09 01:02:49 +0300

    upload hometask

--- Final_project/dags/sql/Staging.sql
+++ Final_project/dags/sql/Staging.sql
@@ -1,25 +1,50 @@
-DROP TABLE IF EXISTS stage;
+DROP TABLE IF EXISTS all_orders;
 
 
-CREATE TABLE stage AS
-SELECT
+CREATE TABLE all_orders AS
+SELECT 
     o.order_id,
-    o.total_amount AS order_total,
-    o.status,
     o.order_date,
-    CONCAT(u.first_name, " ", u.last_name) as full_name,
+    o.delivery_date,
+    o.total_amount,
+    o.status,
+    u.full_name,
     u.email,
     u.phone,
     u.loyalty_status,
-    p.name as product_name,
-    pc.name as category_name
-FROM
-    orders o
-JOIN
-    users u ON o.user_id = u.user_id
-LEFT JOIN
-    orderDetails od ON o.order_id = od.order_id
-LEFT JOIN
-    products p ON od.product_id = p.product_id
-LEFT JOIN
-    productCategories pc ON p.category_id = pc.category_id;
+    od.product_id,
+    p.product_name,
+    c.category_name,
+    od.quantity,
+    od.price_per_unit,
+    od.total_price
+FROM 
+    Orders o
+JOIN 
+    Users u ON o.user_id = u.user_id
+JOIN 
+    OrderDetails od ON o.order_id = od.order_id
+JOIN 
+    Products p ON od.product_id = p.product_id
+JOIN 
+    ProductCategories c ON p.category_id = c.category_id;
+
+
+DROP TABLE IF EXISTS amount_by_user;
+
+CREATE TABLE amount_by_user AS
+SELECT 
+    u.user_id,
+    u.full_name,
+    COUNT(o.order_id) AS total_orders,
+    SUM(o.total_amount) AS total_spent
+FROM 
+    Users u
+LEFT JOIN 
+    Orders o ON u.user_id = o.user_id
+GROUP BY 
+    u.user_id, u.full_name
+ORDER BY 
+    total_spent DESC;
+
+
